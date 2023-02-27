af60b8a9109fc8169e148fb12349ad4c5acacf8d
M1nZof <maxykl38@gmail.com>
2023-02-27 21:04:58 +0300

    Download books with titles

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 import os
+from urllib.parse import urlparse
 
 import requests
+from bs4 import BeautifulSoup
 from requests import HTTPError
+from pathvalidate import sanitize_filename
 
 
-def check_for_redirect(response):
-    if response.history:
-        raise HTTPError
+def check_for_redirect(response, check_len=False):
+    if check_len:
+        if len(response.history) > 1:
+            raise HTTPError
+    else:
+        if response.history:
+            raise HTTPError
 
 
-def download_book():
-    for id in range(1, 11):
-        url = f'https://tululu.org/txt.php?id={id}'
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            check_for_redirect(response)
-            with open(os.path.join('books', f'book{id}.txt'), 'w') as book_file:
-                book_file.write(response.text)
-        except HTTPError:
-            continue
+def download_txt(url, filename, folder='books/'):
+    _, book_id = urlparse(url).query.split('=')
+    sanitazed_filename = f'{sanitize_filename(filename)}.txt'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    try:
+        check_for_redirect(response, check_len=True)
+    except HTTPError:
+        return
+
+    with open(os.path.join(folder, f'{book_id}. {sanitazed_filename}'), 'w') as book:
+        book.write(response.text)
+
+    return f"{folder}{sanitazed_filename}"
+
+
+def download_book(book_id):
+    book_url = f'https://tululu.org/b{book_id}/'
+    book_text_url = f'http://tululu.org/txt.php?id={book_id}'
+
+    response = requests.get(book_url)
+    response.raise_for_status()
+
+    try:
+        check_for_redirect(response)
+    except HTTPError:
+        return
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    title, _, author = soup.find('h1').text.split('   ')
+    download_txt(book_text_url, title)
 
 
 def creating_books_directory():
@@ -29,7 +57,5 @@ def creating_books_directory():
 
 if __name__ == '__main__':
     creating_books_directory()
-    download_book()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for book_id in range(1, 11):
+        download_book(book_id)
