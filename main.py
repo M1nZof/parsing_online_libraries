546b0afd5fc4a4f48391b4c638d5517f648fd8a8
M1nZof <maxykl38@gmail.com>
2023-02-27 22:01:13 +0300

    Download book covers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 
 import requests
 from bs4 import BeautifulSoup
@@ -25,7 +25,7 @@ def download_txt(url, filename, folder='books/'):
     try:
         check_for_redirect(response, check_len=True)
     except HTTPError:
-        return
+        raise HTTPError
 
     with open(os.path.join(folder, f'{book_id}. {sanitazed_filename}'), 'w') as book:
         book.write(response.text)
@@ -47,15 +47,35 @@ def download_book(book_id):
 
     soup = BeautifulSoup(response.text, 'lxml')
     title, _, author = soup.find('h1').text.split('   ')
-    download_txt(book_text_url, title)
+
+    try:
+        download_txt(book_text_url, title)
+        download_image(response)
+    except HTTPError:
+        return
 
 
-def creating_books_directory():
-    if not os.path.exists('books'):
-        os.mkdir('books')
+def download_image(book_response):
+    soup = BeautifulSoup(book_response.text, 'lxml')
+    image_tag = soup.find('div', {'class': 'bookimage'}).select('img')
+    image_endlink = [item['src'] for item in image_tag][0]
+    image_name = image_endlink.split('/')[2]
+    image_link = urljoin('https://tululu.org', image_endlink)
+
+    response = requests.get(image_link)
+    response.raise_for_status()
+
+    with open(os.path.join('images', f'{image_name}'), 'wb') as image:
+        image.write(response.content)
+
+
+def creating_books_directory(name):
+    if not os.path.exists(name):
+        os.mkdir(name)
 
 
 if __name__ == '__main__':
-    creating_books_directory()
+    creating_books_directory('books')
+    creating_books_directory('images')
     for book_id in range(1, 11):
         download_book(book_id)
