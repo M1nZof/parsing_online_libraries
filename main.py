e599a4e7b2dfd56f1fd28221d41d2a4438b233e9
M1nZof <maxykl38@gmail.com>
2023-02-27 15:26:55 +0300

    Added methods download_book and creating_books_directory

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import os
 
 import requests
+from requests import HTTPError
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
 
 
 def download_book():
-    os.mkdir('books')
     for id in range(1, 11):
         url = f'https://tululu.org/txt.php?id={id}'
-        response = requests.get(url)
-        response.raise_for_status()
-        with open(os.path.join('books', f'book{id}.txt'), 'w') as book_file:
-            book_file.write(response.text)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            with open(os.path.join('books', f'book{id}.txt'), 'w') as book_file:
+                book_file.write(response.text)
+        except HTTPError:
+            continue
+
+
+def creating_books_directory():
+    if not os.path.exists('books'):
+        os.mkdir('books')
 
 
 if __name__ == '__main__':
+    creating_books_directory()
     download_book()
 
 
