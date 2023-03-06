890796db419621e070527d14ec2c3d26c24cf44b
M1nZof <maxykl38@gmail.com>
2023-03-06 20:14:46 +0300

    Added validation on book content

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
-    parsed_content = soup.find('div', {'id': 'content'}).find('a', {'class': ''})
-    book_id = parsed_content['href']
-    book_url = urljoin(url, book_id)
-    print(book_url)
+    parsed_content = soup.find('div', {'id': 'content'}).find_all('a', {'class': ''})
+    for book_tag in parsed_content:
+        book_id = book_tag['href']
+        if book_id.startswith('/b'):
+            book_url = urljoin(url, book_id)
+            print(book_url)
