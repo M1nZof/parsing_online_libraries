20c94569743729f2aa17c2d976023ee3a16f3d5c
M1nZof <maxykl38@gmail.com>
2023-03-06 20:20:39 +0300

    Fixed parsing

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -9,9 +9,8 @@ if __name__ == '__main__':
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
-    parsed_content = soup.find('div', {'id': 'content'}).find_all('a', {'class': ''})
+    parsed_content = soup.find('div', {'id': 'content'}).find_all('table', {'class': 'd_book'})
     for book_tag in parsed_content:
-        book_id = book_tag['href']
-        if book_id.startswith('/b'):
-            book_url = urljoin(url, book_id)
-            print(book_url)
+        book_id = book_tag.find_next('a')['href']
+        book_url = urljoin(url, book_id)
+        print(book_url)
