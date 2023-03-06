33652b7747335c58649aadac07067ce11b715aac
M1nZof <maxykl38@gmail.com>
2023-03-06 21:25:46 +0300

    Added arguments for running code

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 import time
@@ -10,10 +11,16 @@ from requests import HTTPError
 from main import parse_book_page, download_txt, check_for_redirect, download_image
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Скрипт загрузки книг научной фантастики с сайта https://tululu.org')
+    parser.add_argument('start_page', help='С какой страницы начинать скачивание книг', type=int, default=1)
+    parser.add_argument('end_page', help='Какой страницей заканчивать скачивание книг', type=int, default=2)
+
+    args = parser.parse_args()
+
     os.makedirs('books', exist_ok=True)
     os.makedirs('images', exist_ok=True)
 
-    for page in range(1, 11):
+    for page in range(args.start_page, args.end_page):
         genre_url = 'http://tululu.org/l55/'
         page_url = urljoin(genre_url, str(page))
         response = requests.get(page_url)
